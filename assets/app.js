919770476f64b032b71cc781007e2a80127b07cc
Eleanor Mark <emark@Eleanors-MBP.attlocal.net>
2017-03-29 14:15:23 -0700

    initialize Firebase

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA_rWBKTvLCXJgZXfqQnYuASgtn2YlDHvI",
+    authDomain: "rps-game-a0e51.firebaseapp.com",
+    databaseURL: "https://rps-game-a0e51.firebaseio.com",
+    storageBucket: "rps-game-a0e51.appspot.com",
+    messagingSenderId: "348895860438"
+  };
+  firebase.initializeApp(config);
